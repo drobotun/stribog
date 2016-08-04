b5c7385034f03242ea9a50532a55fe9c7b61796b
Дроботун Евгений <drobotun@xakep.ru>
2016-08-04 18:03:52 +0400

    Update gost_3411_2012_calc.c

--- gost_3411_2012_calc.c
+++ gost_3411_2012_calc.c
@@ -37,7 +37,7 @@ static void
 GOSTHashAdd512(const uint8_t *a, const uint8_t *b, uint8_t *c)
 {
     int i;
-    int internal = 0;
+    unsigned int internal = 0;
     for (i = 0; i < 64; i++)
     {
         internal = a[i] + b[i] + (internal >> 8);
